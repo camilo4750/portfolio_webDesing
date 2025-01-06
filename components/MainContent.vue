5002ced6f44d438b064a2cb3c9d6e20c07a5d707
Camilo Alarcon <camilo4750@gmail.com>
2025-01-06 18:46:13 -0500

    Fix container distribution

--- components/MainContent.vue
+++ components/MainContent.vue
@@ -1,26 +1,17 @@
 <template>
-  <div
-    class="w-[97%] lg:w-[82%] h-[96%] lg:h-[95%] bg-gray-100 dark:bg-gray-400 rounded-2xl lg:mr-5 overflow-y-auto flex flex-col"
+  <main
+    class="w-[97%] lg:w-[82%] h-[96%] lg:h-[95%] bg-gray-100 dark:bg-gray-400 rounded-2xl lg:mr-5 overflow-hidden grid grid-rows-[auto_1fr]"
   >
-    <div
-      class="sticky w-full h-10 px-1 lg:px-3 lg:p-0 flex justify-end items-center gap-2 top-0 z-10 bg-gray-100 dark:bg-gray-400 border-b"
-    >
-      <UiLanguageToggle />
-      <UiThemeToggle />
-      <UiSidebarToggle />
+    <UiSubmenu />
+    <div class="overflow-y-auto h-full">
+      <SectionAbout />
+      <SectionExperience />
     </div>
-    <!-- contenido principal -->
-    <main class="h-full">
-      <SectionAbout/>
-      <!-- Add more sections here as needed -->
-    </main>
-  </div>
+  </main>
 </template>
 
 <style scoped>
-section {
-  scroll-margin-top: 2.4rem; /* Adjust this value as needed */
-}
+
 
 html {
   scroll-behavior: smooth;
