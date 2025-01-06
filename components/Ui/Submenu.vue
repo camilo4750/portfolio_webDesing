25544ee1083412143387f1ca556c595ae9d610fe
Camilo Alarcon <camilo4750@gmail.com>
2025-01-06 18:43:39 -0500

    Add submenu main

---
+++ components/Ui/Submenu.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="sticky w-full h-10 px-1 lg:px-3 lg:p-0 flex justify-end items-center gap-2 top-0 z-10 bg-gray-100 dark:bg-gray-400 border-b"
+  >
+    <UiLanguageToggle />
+    <UiThemeToggle />
+    <UiSidebarToggle />
+  </div>
+</template>
