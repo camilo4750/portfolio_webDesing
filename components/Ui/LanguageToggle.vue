59b990be179da372ae6e7c1bff196d23d94ae96b
Camilo Alarcon <camilo4750@gmail.com>
2025-02-28 10:19:44 -0500

    Add attribute specification

--- components/Ui/LanguageToggle.vue
+++ components/Ui/LanguageToggle.vue
@@ -1,14 +1,16 @@
 <template>
-  <select
-    name="language"
-    id="language"
-    class="text-xs p-1 rounded bg-slate-300 dark:bg-slate-700 dark:text-white"
-    v-model="value"
-    arial-label="language"
-  >
-    <option value="es">{{ $t("selectSpanish") }}</option>
-    <option value="en">{{ $t("selectEnglish") }}</option>
-  </select>
+  <label for="language">
+    <span class="sr-only">select language</span>
+    <select
+      name="language"
+      id="language"
+      class="text-xs p-1 rounded bg-slate-300 dark:bg-slate-700 dark:text-white"
+      v-model="value"
+    >
+      <option value="es">{{ $t("selectSpanish") }}</option>
+      <option value="en">{{ $t("selectEnglish") }}</option>
+    </select>
+  </label>
 </template>
 
 <script setup lang="ts">
