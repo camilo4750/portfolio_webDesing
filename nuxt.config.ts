e6246ef6eeadb30c47f0a3d902f09e75c8f6b356
Camilo Alarcon <camilo4750@gmail.com>
2025-02-28 10:20:36 -0500

    Add define language

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
+      htmlAttrs: {
+        lang: 'es'
+      },
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
       link: [
