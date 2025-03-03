c983ce78e05c62bbe8785182f7c9b05c3bca09f1
Camilo Alarcon <camilo4750@gmail.com>
2025-03-03 17:28:44 -0500

    Add transition to sidebar

--- components/Sidebar/index.vue
+++ components/Sidebar/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <div
-    v-if="isOpen"
-    class="fixed inset-0 z-50 bg-gray-300 dark:bg-gray-800 px-5 w-full lg:hidden"
-  >
-    <div class="flex justify-end py-3 lg:p-3 dark:text-white">
-      <UiSidebarToggle />
+  <Transition name="sidebar">
+    <div
+      v-if="isOpen"
+      class="fixed inset-0 z-50 bg-gray-300 dark:bg-gray-800 px-5 w-full lg:hidden"
+    >
+      <div class="flex justify-end py-3 lg:p-3 dark:text-white">
+        <UiSidebarToggle />
+      </div>
+      <SidebarMenu />
     </div>
-    <SidebarMenu />
-  </div>
+  </Transition>
   <div
     class="hidden side-menu lg:w-[25%] xl:w-[24%] 2xl:w-[20%] h-full lg:py-5 xl:py-14 px-5 lg:px-5 xl:px-10 lg:flex items-center overflow-y-auto"
   >
@@ -21,14 +23,15 @@ import { useSidebarState } from "@/composables/useSidebarState";
 const { isOpen } = useSidebarState();
 </script>
 
-<style>
-@media screen and (max-width: 1488px) {
-  /* .side-menu {
-    width: 30%;
-    padding-left: 2rem;
-    padding-right: 2rem;
-    padding-top: 2.5rem;
-    padding-bottom: 2.5rem;
-  } */
+<style scoped>
+.sidebar-enter-active,
+.sidebar-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+
+.sidebar-enter-from,
+.sidebar-leave-to {
+  opacity: 0;
+  transform: translateX(-60%);
 }
 </style>
