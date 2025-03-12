7ca6e6b4b4c2f16c098509cfac0290caeed8b262
Camilo Alarcon <camilo4750@gmail.com>
2025-03-12 16:23:12 -0500

    Fix skill description

--- i18n.config.ts
+++ i18n.config.ts
@@ -23,30 +23,37 @@ export default defineI18nConfig(() => ({
       jobPositionSavne: "Desarrollador web",
       juniorSavneExperience: {
         title: "Desarrollador junior",
-        one: "Proporcioné soporte para aplicaciones en PHP 5.0, desarrollando nuevos módulos y corrigiendo errores en los existentes.",
-        two:
-          "Trabajé con aplicaciones más robustas utilizando Laravel (a partir de la versión 6.0) y Vue.js (2 y 3). " +
-          "Implementé servicios externos como SimpliRoute, integré y trabajé con mapas, generé gráficos e informes a gran escala, " +
-          "utilicé colas y envié correos electrónicos de forma automática.",
+        one: "Documenté módulos y funcionalidades mediante diagramas de flujo, secuencia y casos de uso con StarUML y Draw.io.",
+        two: "Proporcioné soporte en aplicaciones en PHP puro, aplicando soluciones modulares y escalables que aumentaron la calidad del código.",
         three:
-          "Desarrollé módulos con alta complejidad lógica, asegurando siempre la satisfacción del cliente.",
-        four:
-          "Colaboré en mejorar la escalabilidad y el mantenimiento interno desarrollando una sólida arquitectura basada en capas " +
-          "adaptada a nuestras necesidades, logrando excelentes resultados en nuevos proyectos.",
+          "Optimicé bases de datos en PostgreSQL, mejorando relaciones, normalizando estructuras y optimizando índices.",
+        four: "Supervisé y mejoré el rendimiento de sitios web mediante Lighthouse, identificando y resolviendo problemas de estabilidad y carga, optimizando recursos y mejorando la experiencia del usuario.",
+        five: "Mejoré la manipulación del DOM y la carga de datos en una aplicación basada en Blade, utilizando jQuery, JavaScript y AJAX",
+        six: "Introduje Vue 3 (Options API) en plantillas Blade, optimizando módulos dinámicos y reduciendo tiempos de carga.",
+        seven:
+          "Convertí diseños de Figma a código con HTML, CSS y Bootstrap, logrando interfaces pixel-perfect y 100% responsivas. La alta calidad del desarrollo y la satisfacción del cliente me llevaron a un ascenso a Desarrollador Semi Senior.",
       },
       semiSeniorSavneExperience: {
         title: "Desarrollador semi-senior",
-        one: "Me eligieron como líder para desarrollar un sistema completo, gestionando a dos programadores.",
-        two: "Diseñé modelos C4 para representar la arquitectura y los extendí al nivel de clase.",
+        one: "Mantuve comunicación directa con clientes, traduciéndo sus necesidades en soluciones tecnológicas funcionales.",
+        two: "Gestioné múltiples proyectos simultáneamente, optimizando flujos de trabajo y asegurando entregas eficientes dentro de los plazos establecidos.",
         three:
-          "Resolví la duplicación de código en el backend utilizando servicios.",
-        four: "Como equipo, desarrollamos una arquitectura hexagonal para el backend, aprovechando también la arquitectura basada en capas que ya teníamos.",
-        five:
-          "Implementé nuevos proyectos separando el backend para usarlo como API con JWT. En el frontend, migramos a Nuxt.js con TypeScript " +
-          "por su escalabilidad y alineación con nuestras necesidades.",
-        six: "Implementamos una arquitectura frontend sostenible y escalable para proyectos de alto potencial.",
+          "Diseñé e implementé arquitecturas escalables en Laravel, aplicando una estructura en capas con los patrones Repository y Service",
+        four: "Utilicé modelos C4 en StarUML para documentar la arquitectura del sistema y definir estructuras a nivel de clases.",
+        five: "Desarrollé sistemas completos desde cero con Laravel y Vue 3, utilizando Docker y PostgreSQL para garantizar escalabilidad en entornos de alto tráfico.",
+        six: "Implementé pruebas unitarias, de integración y funcionales con PHPUnit, empleando Mocking para simular dependencias y asegurar una cobertura del 100% en módulos clave.",
         seven:
-          "Implementación de i18n para la creación de aplicaciones bilingües.",
+          "Lideré la mentoría de desarrolladores junior, realizando revisiones de código en GitLab, corrigiendo errores en lógica e implementación, y proporcionando retroalimentación para mejorar la calidad del código.",
+        eight:
+          "Apliqué autenticación con JWT en Laravel dentro de una arquitectura desacoplada, reforzando la seguridad del sistema y reduciendo vulnerabilidades",
+        nine: "Participé en la implementación de una arquitectura modular para una API REST con Nuxt 3, Nuxt UI, TypeScript y TailwindCSS.",
+        ten: "Diseñé y codifique componentes modulares y eficientes.",
+        eleven:
+          "Optimizé el rendimiento del sistema aplicando estrategias como lazy loading, caching y SSR/ISR.",
+        twelve:
+          "Configuré y personalicé i18n para soportar aplicaciones bilingües, ampliando el alcance del sistema y mejorando la accesibilidad.",
+        thirteen:
+          "Inglés Intermedio (B1) – Capacidad para mantener conversaciones y leer documentación técnica.",
       },
       projects: {
         title: "Mis creaciones más recientes",
@@ -119,29 +126,34 @@ export default defineI18nConfig(() => ({
       titleSectionExperience: "My experience",
       jobPositionSavne: "Web developer",
       juniorSavneExperience: {
-        title: "Junior Developer",
-        one: "I provided support for applications in PHP 5.0, developing new modules and fixing bugs in existing ones.",
-        two:
-          "Worked with more robust applications using Laravel (from version 6.0) and Vue.js (2 and 3). I implemented external services such as SimpliRoute, " +
-          "integrated and worked with maps, generated charts and large-scale reports, utilized Queues, and automated email sending.",
+        title: "Developer Junior",
+        one: "Documented modules and functionalities using flowcharts, sequence diagrams, and use case diagrams with StarUML and Draw.io.",
+        two: "Provided support for applications in pure PHP, applying modular and scalable solutions that increased code.",
         three:
-          "I developed modules with high logical complexity, always ensuring customer satisfaction.",
-        four:
-          " I collaborated in improving scalability and internal maintenance by developing a solid layer-based architecture tailored to our needs, " +
-          "achieving excellent results in new projects.",
+          "Optimized PostgreSQL databases by improving relationships, normalizing structures, and optimizing indexes.",
+        four: "Monitored and enhanced website performance using Lighthouse, identifying and resolving stability and loading issues, optimizing resources, and improving user experience.",
+        five: "Improved DOM manipulation and data loading in a Blade-based application using jQuery, JavaScript, and AJAX.",
+        six: "Introduced Vue 3 (Options API) in Blade templates, optimizing modules and reducing load times",
+        seven:
+          "Converted Figma designs into code using HTML, CSS, and Bootstrap, achieving pixel-perfect and fully responsive interfaces. The high development quality and client satisfaction led to my promotion to Semi-Senior Developer.",
       },
       semiSeniorSavneExperience: {
-        title: "Semi-senior development",
-        one: "I was chosen as the leader to develop a complete system, managing two programmers.",
-        two: "I designed C4 models to represent the architecture and extended them to the class level.",
+        title: "developer Semi-Senior",
+        one: "Maintained direct communication with clients, translating their needs into functional technological solutions",
+        two: "Managed multiple projects simultaneously, optimizing workflows and ensuring efficient deliveries within established deadlines.",
         three:
-          "I resolved code duplication in the backend by utilizing services.",
-        four: "As a team, we developed a hexagonal architecture for the backend, also making use of the layer-based architecture we already had.",
+          "Designed and implemented scalable architectures in Laravel, applying a layered structure with the Repository and Service patterns.",
+        four: "Used C4 models in StarUML to document system architecture and define class-level structures",
         five:
-          "I implemented new projects by separating the backend to be used as an API with JWT. On the frontend, we migrated to Nuxt.js with TypeScript " +
-          "for its scalability and alignment with our needs.",
-        six: "We implemented a sustainable and scalable frontend architecture for high-potential projects.",
-        seven: "Implementation of i18n for creating bilingual applications.",
+          "Developed complete systems from scratch using Laravel and Vue 3, leveraging Docker and PostgreSQL to ensure scalability in high-traffic environments.",
+        six: "Implemented unit, integration, and functional tests with PHPUnit, utilizing Mocking to simulate dependencies and ensuring 100% coverage in key modules.",
+        seven: "Led mentorship for junior developers, conducting code reviews in GitLab, correcting logic and implementation errors, and providing feedback to enhance code quality.",
+        eight: "Applied JWT authentication in Laravel within a decoupled architecture, strengthening system security and reducing vulnerabilities ",
+        nine: "Contributed to the implementation of a modular architecture for a REST API with Nuxt 3, Nuxt UI, TypeScript, and TailwindCSS.",
+        ten: "Design and coded modular and efficient components.",
+        eleven: "Optimized system performance by applying strategies such as lazy loading, caching, and SSR/ISR.",
+        twelve: "Configured and customized i18n to support bilingual applications, expanding system reach and enhancing accessibility. ",
+        thirteen: "Intermediate English (B1) – Ability to hold conversations and read technical documentation.",
       },
       projects: {
         title: "My latest creations",
@@ -196,6 +208,5 @@ export default defineI18nConfig(() => ({
       },
       footer: "All rights reserved",
     },
-
   },
 }));
