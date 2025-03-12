44612eb23d469673954193154a03d77db830fc37
Camilo Alarcon <camilo4750@gmail.com>
2025-03-12 16:25:40 -0500

    Add new experience sections

--- components/Section/Experience.vue
+++ components/Section/Experience.vue
@@ -19,6 +19,65 @@
       </p>
       <div class="mb-10 ms-4 list-none">
         <div
+          class="absolute w-3 h-3 bg-customGreen700 rounded-full mt-1.5 -start-1.5 border border-black dark:border-white"
+        >
+          <span
+            class="animate-ping absolute inline-flex h-full w-full rounded-full bg-customGreen600 opacity-75"
+          ></span>
+        </div>
+        <time
+          class="mb-1 text-sm font-normal leading-none text-customGreen500 dark:text-customGreen800"
+          >Abril 2023 - Noviembre 2024
+        </time>
+        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+          {{ $t("semiSeniorSavneExperience.title") }}
+        </h3>
+        <div class="mb-4 text-base font-normal text-black dark:text-gray-950">
+          <ul class="list-disc list-outside ml-4 animateRight__">
+            <li>
+              {{ $t("semiSeniorSavneExperience.one") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.two") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.three") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.four") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.five") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.six") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.seven") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.eight") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.nine") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.ten") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.eleven") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.twelve") }}
+            </li>
+            <li>
+              {{ $t("semiSeniorSavneExperience.thirteen") }}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="mb-10 ms-4 list-none">
+        <div
           class="absolute w-3 h-3 bg-customGreen700 rounded-full mt-1.5 -start-1.5 border border-black dark:border-white dark:bg-customGreen700"
         >
           <span
@@ -46,46 +105,14 @@
             <li>
               {{ $t("juniorSavneExperience.four") }}
             </li>
-          </ul>
-        </div>
-      </div>
-      <div class="mb-10 ms-4 list-none">
-        <div
-          class="absolute w-3 h-3 bg-customGreen700 rounded-full mt-1.5 -start-1.5 border border-black dark:border-white"
-        >
-          <span
-            class="animate-ping absolute inline-flex h-full w-full rounded-full bg-customGreen600 opacity-75"
-          ></span>
-        </div>
-        <time
-          class="mb-1 text-sm font-normal leading-none text-customGreen500 dark:text-customGreen800"
-          >abril 2023 - octubre 2024
-        </time>
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-          {{ $t("semiSeniorSavneExperience.title") }}
-        </h3>
-        <div class="mb-4 text-base font-normal text-black dark:text-gray-950">
-          <ul class="list-disc list-outside ml-4 animateRight__">
             <li>
-              {{ $t("semiSeniorSavneExperience.one") }}
+              {{ $t("juniorSavneExperience.five") }}
             </li>
             <li>
-              {{ $t("semiSeniorSavneExperience.two") }}
+              {{ $t("juniorSavneExperience.six") }}
             </li>
             <li>
-              {{ $t("semiSeniorSavneExperience.three") }}
-            </li>
-            <li>
-              {{ $t("semiSeniorSavneExperience.four") }}
-            </li>
-            <li>
-              {{ $t("semiSeniorSavneExperience.five") }}
-            </li>
-            <li>
-              {{ $t("semiSeniorSavneExperience.six") }}
-            </li>
-            <li>
-              {{ $t("semiSeniorSavneExperience.seven") }}
+              {{ $t("juniorSavneExperience.seven") }}
             </li>
           </ul>
         </div>
