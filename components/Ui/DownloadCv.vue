7368a09a368d8227ac9f3b1ff96e8f95e035fa16
Camilo Alarcon <camilo4750@gmail.com>
2025-03-19 16:34:15 -0500

    Update CV in Spanish

--- components/Ui/DownloadCv.vue
+++ components/Ui/DownloadCv.vue
@@ -1,12 +1,16 @@
 <script setup>
 const { locale } = useI18n();
 
-const currentLocale = computed(() => locale.value);
+const href = computed(() => {
+  return locale.value === "es"
+    ? "/CV_Camilo_Alarcon_Desarrollador_Software.pdf"
+    : "/Camilo_Alarcon_Software_Developer_EN.pdf";
+});
 </script>
 
 <template>
   <a
-    :href="currentLocale === 'es' ? '/Camilo_Alarcon_Desarrollador_Software_ES.pdf' : '/Camilo_Alarcon_Software_Developer_EN.pdf'"
+    :href="href"
     download
     class="bg-[#c79825] hover:bg-[#d29b17] text-white flex items-center justify-center gap-2 font-semibold max-w-[180px] rounded-full py-1.5 px-4 shadow hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:-translate-y-0.5 hover:scale-110"
   >
